ba1b3f3c43ee0bf3a2e6a51c489974afe015b8b3
destino92 <destinodello@gmail.com>
2016-04-19 16:16:01 +0300

    changed products index page layout using bootstrap grid system and thumbnails

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "300x" }, default_url: "./assets/images/product_image.gif"
+  has_attached_file :image, default_url: "./assets/images/product_image.gif"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
