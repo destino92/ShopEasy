de7a53076a22776ba35f757194c611a8693670d3
destino92 <destinodello@gmail.com>
2016-04-19 23:09:49 +0300

    Finished dropbox set up

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ActiveRecord::Base
-  has_attached_file :image, default_url: "./assets/images/product_image.gif",
-  					:storage => :dropbox,
-    				:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  if Rails.env.development?
+  	has_attached_file :image, styles: => { :medium => "200x"}, default_url: "./assets/images/product_image.gif"
+  else
+  	has_attached_file :image, styles: => { :medium => "200x"}, default_url: "./assets/images/product_image.gif",
+  					  :storage => :dropbox,
+    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    				  :path => ":style/:id_:filename"
+  end
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
